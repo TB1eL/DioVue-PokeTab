a65d9663c1d16befaa00d5783f9a077f97db5d8e
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-06-11 14:54:01 -0300

    feat(PokemonCard): props, computed de numero, nome, cores e delay

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -13,3 +13,33 @@
         </div>
     </router-link>
 </template>
+
+<script>
+    import TypeBadge from './TypeBadge.vue'
+    import { artworkUrl, spriteUrl } from '../api/pokeapi.js'
+    import { pad, prettyName, typeColor } from '../utils/format.js'
+
+    export default {
+        name: 'PokemonCard',
+        components: { TypeBadge },
+        props: {
+            pokemon: { type: Object, required: true },
+            index: { type: Number, default: 0 }
+        },
+
+        data() {
+            return { src: artworkUrl(this.pokemon.id), triedSprite: false }
+        },
+
+        computed: {
+            artwork() { return this.src },
+            number() { return pad(this.pokemon.id) },
+            display() { return prettyName(this.pokemon.name) },
+            accent() { return typeColor(this.pokemon.types?.[0]) },
+            accent2() { return typeColor(this.pokemon.types?.[1] || this.pokemon.types?.[0]) },
+            delay() { return Math.min(this.index, 11) * 45 }
+        }
+    }
+
+    
+</script>
