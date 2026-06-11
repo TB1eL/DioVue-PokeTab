1a95ce4a818a0cded3b464c8ef4da118fc5a7c0e
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-06-11 13:01:26 -0300

    feat(TypeBadge): lógica de props e computed (cor e label por tipo)

--- src/components/TypeBadge.vue
+++ src/components/TypeBadge.vue
@@ -3,3 +3,18 @@
     <span class="dot"></span>{{ label }}
   </span>
 </template>
+
+<script>
+import { typeColor, typeLabel } from '../utils/format.js'
+export default {
+  name: 'TypeBadge',
+  props: {
+    type: String,
+    small: Boolean
+  },
+  computed: {
+    color() { return typeColor(this.type) },
+    label() { return typeLabel(this.type) }
+  }
+}
+</script>
