ab9d83c6dbb782ea9c967e8e48f4821d62099da6
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-06-11 16:53:14 -0300

    feat(TypePage): props, computed de geracao e filtro, methods de toggle e sprite

--- src/components/TypePage.vue
+++ src/components/TypePage.vue
@@ -30,3 +30,53 @@
         <p class="empty" v-else>Nenhum Pokémon encontrado para "{{ search }}".</p>
   </div>
 </template>
+
+<script>
+    import PokemonCard from './PokemonCard.vue'
+
+    export default {
+        name: 'TypePage',
+        components: { PokemonCard },
+        props: {
+            title: String,
+            icon: String,
+            accent: String,
+            glow: String,
+            pokemon: Array
+        },
+
+        data() {
+            return {
+            search: '',
+            activeGen: null
+            }
+        },
+
+        computed: {
+            generations() {
+            const set = new Set(this.pokemon.map(p => p.gen))
+            return [...set].sort()
+            },
+            filtered() {
+            return this.pokemon.filter(p => {
+                const q = this.search.toLowerCase()
+                const matchSearch = p.name.toLowerCase().includes(q) || p.number.includes(q)
+                const matchGen = !this.activeGen || p.gen === this.activeGen
+                return matchSearch && matchGen
+            })
+            }
+        },
+        
+        methods: {
+            toggleGen(g) {
+            this.activeGen = this.activeGen === g ? null : g
+            },
+            spriteUrl(id) {
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+            },
+            spriteBigUrl(id) {
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+            }
+        }
+    }
+</script>
