2f1b95a32edb7c92f6d3269e8a8093b5c8f8aeab
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-06-11 14:46:27 -0300

    feat(PokemonCard): template com router-link, imagem e TypeBadge

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link :to="`/pokemon/${pokemon.name}`" class="card rise" :style="{ '--accent': accent, '--accent2': accent2, animationDelay: delay + 'ms' }">
+        <span class="num">{{ number }}</span>
+        <div class="img-wrap">
+            <div class="aura"></div>
+            <img :src="artwork" :alt="display" class="art" loading="lazy" @error="onError"/>
+        </div>
+
+        <h3 class="name">{{ display }}</h3>
+
+        <div class="types">
+            <TypeBadge v-for="t in pokemon.types" :key="t" :type="t" small />
+        </div>
+    </router-link>
+</template>
