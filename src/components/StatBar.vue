ca4585790cd6fdc537f83cf5b589d6237be36c71
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-06-11 13:36:28 -0300

    feat(StatBar): estrutura base do template com grid de stat

---
+++ src/components/StatBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="stat-row">
+        <span class="stat-name">{{ label }}</span>
+        <span class="stat-value">{{ value }}</span>
+        <div class="stat-bar-bg">
+            <div class="stat-bar-fill" :style="{ width: percent + '%', background: color }"></div>
+        </div>
+  </div>
+</template>
