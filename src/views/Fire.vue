29025aa7fd181d14f04cfab93f5135d7ddb26711
joaofortkamp <engs-joaofortkamp@camporeal.edu.br>
2026-06-11 22:32:53 -0300

    feat(pages)_ pagina de Pokemon de Fogo

---
+++ src/views/Fire.vue
@@ -0,0 +1,20 @@
+<template>
+  <TypePage
+    title="Pokémon de Fogo"
+    icon="🔥"
+    accent="#f44336"
+    glow="rgba(244,67,54,0.15)"
+    :pokemon="FIRE_STARTERS"
+  />
+</template>
+
+<script>
+import TypePage from '../components/TypePage.vue'
+import { FIRE_STARTERS } from '../data/pokemon.js'
+
+export default {
+  name: 'Fire',
+  components: { TypePage },
+  data() { return { FIRE_STARTERS } }
+}
+</script>
