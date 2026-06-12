b0085c3e4ff9f60375043f360d9179aeb5132761
joaofortkamp <engs-joaofortkamp@camporeal.edu.br>
2026-06-11 22:32:17 -0300

    feat(pages)_ pagina de Pokemon de Planta

---
+++ src/views/Grass.vue
@@ -0,0 +1,20 @@
+<template>
+  <TypePage
+    title="Pokémon de Planta"
+    icon="🌿"
+    accent="#4caf50"
+    glow="rgba(76,175,80,0.15)"
+    :pokemon="GRASS_STARTERS"
+  />
+</template>
+
+<script>
+import TypePage from '../components/TypePage.vue'
+import { GRASS_STARTERS } from '../data/pokemon.js'
+
+export default {
+  name: 'Grass',
+  components: { TypePage },
+  data() { return { GRASS_STARTERS } }
+}
+</script>
