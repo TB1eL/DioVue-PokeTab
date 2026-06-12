3cee334cca6a23d87a327b0692461089c76ad7e1
joaofortkamp <engs-joaofortkamp@camporeal.edu.br>
2026-06-11 22:33:20 -0300

    feat(pages)_ pagina de Pokemon de Agua

---
+++ src/views/Water.vue
@@ -0,0 +1,20 @@
+<template>
+  <TypePage
+    title="Pokémon de Água"
+    icon="💧"
+    accent="#2196f3"
+    glow="rgba(33,150,243,0.15)"
+    :pokemon="WATER_STARTERS"
+  />
+</template>
+
+<script>
+import TypePage from '../components/TypePage.vue'
+import { WATER_STARTERS } from '../data/pokemon.js'
+
+export default {
+  name: 'Water',
+  components: { TypePage },
+  data() { return { WATER_STARTERS } }
+}
+</script>
